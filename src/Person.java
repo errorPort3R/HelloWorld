d9ad22c43e88d9f9607e47c142fd351912efd33d
jeffryporter <jporter7@gmail.com>
2016-05-16 11:52:30 -0400

    Initial import

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jeffryporter on 5/16/16.
+ * AM classwork
+ */
+public class Person {
+    //declare variables
+    String name;
+    int age;
+    boolean isAlive;
+    Person[] family = new Person[10];
+
+}
