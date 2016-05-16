f595b165db1d7e7b4bc324fc75fa94796b57410a
jeffryporter <jporter7@gmail.com>
2016-05-16 13:03:04 -0400

    Add animal class

--- src/Person.java
+++ src/Person.java
@@ -4,9 +4,56 @@
  */
 public class Person {
     //declare variables
-    String name;
+    String firstName;
+    String lastName;
     int age;
-    boolean isAlive;
+    boolean isAlive = true;
     Person[] family = new Person[10];
 
+    //constructors
+    public Person(String firstName, String lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //methods
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getName()
+    {
+        return firstName + " " + lastName;
+    }
+
+    public void setFirstName(String newName)
+    {
+        firstName = newName;
+    }
+
+    public void setLastName(String newName)
+    {
+        lastName = newName;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setAge(int newAge)
+    {
+        age = newAge;
+    }
+
+    public void hasDied()
+    {
+        isAlive = false;
+    }
 }
---
+++ src/Student.java
@@ -0,0 +1,40 @@
+/**
+ * Created by jeffryporter on 5/16/16.
+ */
+public class Student {
+    //declare variables
+    private Person person;
+    private final int id;
+    private double gpa;
+    private int creditHoursTaken;
+    private static int CURRENT_ID_NUMBER = 10110;
+
+    //constructor
+    public Student(String firstName, String lastName)
+    {
+        person = new Person(firstName, lastName);
+        id = CURRENT_ID_NUMBER;
+        CURRENT_ID_NUMBER++;
+    }
+
+    public double getGPA()
+    {
+        return gpa;
+    }
+
+    public void setGPA(double newGPA)
+    {
+        gpa = newGPA;
+    }
+
+    public int getCreditHoursTaken()
+    {
+        return creditHoursTaken;
+    }
+
+    public void setCreditHoursTaken(int creditHours)
+    {
+        creditHoursTaken = creditHours;
+    }
+
+}
